78d96ec5fcaabc73923be7d952fdfa8a518cb4d1
brianotoole <botoole518@gmail.com>
2015-08-04 19:13:57 -0400

    class list page template

--- wp-content/themes/mjpaa/part-classes.php
+++ wp-content/themes/mjpaa/part-classes.php
@@ -13,19 +13,22 @@ $loop = new WP_Query( $args );
 if ( $loop->have_posts() ) {
 	while ( $loop->have_posts() ) {
 		$loop->the_post(); ?>
-			<div class="col-sm-2 no-padding img">
-			  <?php if (has_post_thumbnail( $post->ID ) ): //if featured image is uploaded... ?>
-			  <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); $image = $image[0]; ?>
-			  <img class="thumbnail" src="<?php echo $image; ?>">
-			  <?php else: //if no featured image is uploaded, show default icon img ?>
-			  <div class="thumbnail default"></div>
-			  <?php endif; ?>
-			</div>
-			<div class="col-sm-10 descrip">
-			  <h3 class="news-title"><?php the_title(); ?></h3>
-			  <p class="date"><?php echo get_the_date( 'l / F d / Y' ); ?></p>  
-			  <p><?php the_excerpt() ?></p>
-			</div>
+			<div class="class-list">
+				<div class="col-sm-4">
+				  <?php if (has_post_thumbnail( $post->ID ) ): //if featured image is uploaded... ?>
+				  <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); $image = $image[0]; ?>
+				  <img class="thumbnail" src="<?php echo $image; ?>">
+				  <?php else: //if no featured image is uploaded, show default icon img ?>
+				  <div class="thumbnail default"><i class="fa fa-music"></i></div>
+				  <?php endif; ?>
+				</div><!--/.col-->
+				<div class="col-sm-8 descrip">
+				  <h3 class="class-title"><?php the_title(); ?></h3>
+				  <p class="date"><?php the_field('class_start_date'); ?> - <?php the_field('class_end_date'); ?></p>  
+				  <p><?php the_excerpt() ?></p>
+				</div><!--/.col-->
+				<div class="clear"></div><hr />
+			</div><!--/.class-list-->
 <?php } 
 } else {
 	// no posts found
