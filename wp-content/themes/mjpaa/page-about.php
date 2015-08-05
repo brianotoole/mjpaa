f0dfb3bd2bf28b145c0c60a348fc3f470f416dcf
brianotoole <botoole518@gmail.com>
2015-08-05 17:54:29 -0400

    template-about markup

---
+++ wp-content/themes/mjpaa/page-about.php
@@ -0,0 +1,92 @@
+<?php
+/**
+Template Name: Page - About
+ */
+get_header(); ?> 
+            
+	<header class="entry-header">
+	</header><!-- .entry-header -->
+
+<section class="history">
+	<div class="intro row" id="contain">
+		<div class="col-sm-3 visit">
+			<p>Visit</p>
+			  <a href="#">Our Company History</a>
+			<hr />
+			<p>Visit</p>
+			  <a href="#">Our Alumni Spotlight</a>
+		</div><!--/.col-->
+		<div class="col-sm-9 quotes b-left">
+			<h2 class="section-title">Celebrating 35 Years as the Most Reputable<br />
+			Performing Arts Academy in Tampa Bay
+			</h2> 
+			<h5 class="breakout">Voted one of the top 50 dance schools in the country.</h5>
+			<p>Currently one of the Federation of Dance Competition's Top 25 schools in the Southeast Region, FDC People's Choice Award Winner, power ranked with the World Dance Championship Series, Victory Cup Winner and FDC Studio of Excellence.</p>
+		</div><!--/.col-->
+	</div><!--/.intro-->
+</section><!--/.history-->
+
+<section class="about">
+	<div class="intro row" id="contain">
+		<div class="col-sm-8">
+			<h4 class="section-title">The Academy Offers a Complete Performing Arts Curriculum</h4> 
+			<h6>Our program includes...</h6>
+			
+			<div class="program-titles">
+			  <div class="col-sm-2">
+				<i class="fa fa-ticket"></i>
+			  </div>
+			  <div class="col-sm-10">
+				<h5>Performing Arts</h5>
+				<p>fdsafdsafdsaf sdafsdfdsf dfdsfds dsfsdagsdagsdagdsgdsgsd</p>
+			  </div><hr class="visible-xs">
+			  <div class="clear"></div>
+			  
+			  <div class="col-sm-2">
+				<i class="fa fa-music"></i>
+			  </div>
+			  <div class="col-sm-10">
+				<h5>Dance &amp; Classical Ballet</h5>
+				<p>fdsafdsafdsaf sdafsdfdsf dfdsfds dsfsdagsdagsdagdsgdsgsd</p>
+			  </div><hr class="visible-xs">
+			  <div class="clear"></div>
+			  
+			  <div class="col-sm-2">
+				<i class="fa fa-quote-left"></i>
+			  </div>
+			  <div class="col-sm-10">
+				<h5>Musical Theater</h5>
+				<p>fdsafdsafdsaf sdafsdfdsf dfdsfds dsfsdagsdagsdagdsgdsgsd</p>
+			  </div><hr class="visible-xs">
+			  <div class="clear"></div>
+			  
+			  <div class="col-sm-2">
+				<i class="fa fa-video-camera"></i>
+			  </div>
+			  <div class="col-sm-10">
+				<h5>Acting</h5>
+				<p>fdsafdsafdsaf sdafsdfdsf dfdsfds dsfsdagsdagsdagdsgdsgsd</p>
+			  </div><hr class="visible-xs">
+			  <div class="clear"></div>
+			  
+			  <div class="col-sm-2">
+				<i class="fa fa-volume-up"></i>
+			  </div>
+			  <div class="col-sm-10">
+				<h5>Voice</h5>
+				<p>fdsafdsafdsaf sdafsdfdsf dfdsfds dsfsdagsdagsdagdsgdsgsd</p>
+			  </div><hr class="visible-xs">
+			  <div class="clear"></div>
+			</div><!--/.program-titles-->
+			  
+			  
+		</div>
+		<div class="col-sm-4 latest-news">
+			<h4 class="latest-title">News &amp; Events</h4>
+			<?php get_template_part( 'part', 'latest_news' ); ?>
+		</div><!--/.col-->
+	</div><!--/.intro-->
+</section><!--/.about-->
+
+
+	<?php get_footer(); ?>
--- wp-content/themes/mjpaa/page.php
+++ wp-content/themes/mjpaa/page.php
@@ -1,25 +1,25 @@
 <?php
 /**
-Default interior pg
- *
+Default interior page (with sidebar)
  */
-
 get_header(); ?> 
+            
+	<header class="entry-header">
+	</header><!-- .entry-header -->
 
-   <header class="entry-header">
-   </header><!-- .entry-header -->
-    
 	<div class="grid">
-		<div id="primary" class="col-1-1">
+	<?php get_sidebar(); ?>
+		<div id="primary" class="col-9-12">
 			<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
-				
+
 				<?php get_template_part( 'content', 'page' ); ?>
 
 			<?php endwhile; // end of the loop. ?>
 
 			</main><!-- #main -->
 		</div><!-- #primary -->
+
 	</div><!-- grid -->
-<?php get_footer(); ?>
+	<?php get_footer(); ?>
