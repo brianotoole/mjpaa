0eb21a9e3c1bf23479ff30276597977ca1dfb2a9
brianotoole <botoole518@gmail.com>
2015-08-06 16:33:58 -0400

    404 page

--- wp-content/themes/mjpaa/404.php
+++ wp-content/themes/mjpaa/404.php
@@ -7,31 +7,31 @@ get_header(); ?>
 <META NAME="ROBOTS" CONTENT="NOINDEX, FOLLOW">
 <META NAME="ROBOTS" CONTENT="INDEX, NOFOLLOW">
 <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
+	<header class="entry-header">
+	</header><!-- .entry-header -->
 
-		<div class="sec1">
-			<div class="intro" id="contain">
-			<h3 class="h-title"><span>This Page Does Not Exist - 404</span></h3> 
-				 <p class="intro-no-p"></p>
-			</div><!--/.intro-->
-		</div><!--/.sec1-->
-        
-	<div class="grid grid-pad">
-		<div id="primary" class="content-area col-1-1">
-			<main id="main" class="site-main" role="main">
-			<div class="entry-content text-center">
-				<p>Please <a href="<?php bloginfo('url'); ?>">click here to return to the homepage</a>.</p>
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-				<br />
-			</div>
+	<div class="page-about default">
+	<section class="history">
+		<div class="intro row" id="contain">
+			<div class="col-sm-12">
+				<h3 class="section-title"><span>This Page Does Not Exist - 404</span></h3>
+			</div><!--/.col-->
+		</div><!--/.intro-->
+	</section><!--/.history-->
+	
+	<section class="about">
+		<div class="intro row" id="contain">
+			<div class="col-sm-8">
+					<div class="entry-content">
+					  <p><a href="<?php bloginfo('url'); ?>">Click here to return to the homepage</a>.</p>
+					</div>
+			</div><!--/.col-->
+			
+			<div class="col-sm-4 latest-news">
+				<?php get_template_part( 'part', 'latest_news' ); ?>
+			</div><!--/.col-->
+		</div><!--/.intro-->
+	</section><!--/.about-->
+	</div><!--/.page-about-->
 
-			</main><!-- #main -->
-		</div><!-- #primary -->
-	</div><!-- grid -->
-<?php get_footer(); ?>
-
+	<?php get_footer(); ?>
--- wp-content/themes/mjpaa/page.php
+++ wp-content/themes/mjpaa/page.php
@@ -7,33 +7,33 @@ get_header(); ?>
 	<header class="entry-header">
 	</header><!-- .entry-header -->
 
-<div class="page-about default">
-<section class="history">
-	<div class="intro row" id="contain">
-		<div class="col-sm-12">
-			<h3 class="section-title"><?php the_title(); ?></h3>
-			<?php if( get_field('interior_callout') ): ?> 
-			  <p><?php the_field('interior_callout'); ?></p>
-			<?php endif ; ?>
-		</div><!--/.col-->
-	</div><!--/.intro-->
-</section><!--/.history-->
-
-<section class="about">
-	<div class="intro row" id="contain">
-		<div class="col-sm-8">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<div class="entry-content">
-					<?php the_content(); ?>
-				</div>
-			<?php endwhile; // end of the loop. ?>
-		</div><!--/.col-->
-		
-		<div class="col-sm-4 latest-news">
-			<?php get_sidebar(); ?>
-		</div><!--/.col-->
-	</div><!--/.intro-->
-</section><!--/.about-->
-</div><!--/.page-about-->
+	<div class="page-about default">
+	<section class="history">
+		<div class="intro row" id="contain">
+			<div class="col-sm-12">
+				<h3 class="section-title"><?php the_title(); ?></h3>
+				<?php if( get_field('interior_callout') ): ?> 
+				  <p><?php the_field('interior_callout'); ?></p>
+				<?php endif ; ?>
+			</div><!--/.col-->
+		</div><!--/.intro-->
+	</section><!--/.history-->
+	
+	<section class="about">
+		<div class="intro row" id="contain">
+			<div class="col-sm-8">
+				<?php while ( have_posts() ) : the_post(); ?>
+					<div class="entry-content">
+						<?php the_content(); ?>
+					</div>
+				<?php endwhile; // end of the loop. ?>
+			</div><!--/.col-->
+			
+			<div class="col-sm-4 latest-news">
+				<?php get_sidebar(); ?>
+			</div><!--/.col-->
+		</div><!--/.intro-->
+	</section><!--/.about-->
+	</div><!--/.page-about-->
 
 	<?php get_footer(); ?>
