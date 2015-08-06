25ae6ae858a886c2234a601e3cba760de61a2a76
brianotoole <botoole518@gmail.com>
2015-08-06 12:23:19 -0400

    single conditions

--- wp-content/themes/mjpaa/single.php
+++ wp-content/themes/mjpaa/single.php
@@ -14,7 +14,6 @@ get_header(); ?>
 	<div class="page-about">
 	<section class="history">
 		<div class="intro row" id="contain">
-		 <!-- <h3 class="section-title">Our History</h3> -->
 			<div class="col-sm-3 visit">
 				<?php if ( is_singular( 'class' ) ) :  ?>
 				  <p>Program</p>
@@ -26,14 +25,26 @@ get_header(); ?>
 					<span>
 				  <hr />
 				  <p>Grade Level</p>
-				    <span>level</span>
+				    <span>
+				    <?php $grades = get_the_terms( $post->ID , 'grade' );
+						foreach ( $grades as $grade ) {		
+						echo $grade->name; }
+					?>
+				    </span>
 				<?php endif ; ?>
-				<?php if ( is_singular( 'post' ) ) :?>
-				  <p>Visit</p>
-				    <a href="#">spotlight link 1</a>
+				<?php if ( is_singular( 'post' ) && in_category( 'event' ) ) :?>
+				  <p>Date</p>
+				    <span>event date</span>
 				  <hr />
-				  <p>Visit</p>
-				    <a href="#">spotlight link 2</a>
+				  <p>Location</p>
+				    <a href="#">location link</a>
+				<?php endif ; ?>
+				<?php if ( is_singular( 'post' ) && in_category( 'news' ) || in_category() ) :?>
+				  <p>Posted on</p>
+				    <span>published date</span>
+				  <hr />
+				  <p>Posted by</p>
+				    <span>author username</span>
 				<?php endif ; ?>
 			</div><!--/.col-->
 			<div class="col-sm-9 quotes b-left">
