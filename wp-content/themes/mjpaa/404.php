f402a80b22b6fc9efc196234a0fde44272445d8b
brianotoole <botoole518@gmail.com>
2015-08-06 17:00:33 -0400

    added search filter and bar to 404

--- wp-content/themes/mjpaa/404.php
+++ wp-content/themes/mjpaa/404.php
@@ -10,28 +10,37 @@ get_header(); ?>
 	<header class="entry-header">
 	</header><!-- .entry-header -->
 
-	<div class="page-about default">
-	<section class="history">
-		<div class="intro row" id="contain">
-			<div class="col-sm-12">
-				<h3 class="section-title"><span>This Page Does Not Exist - 404</span></h3>
-			</div><!--/.col-->
-		</div><!--/.intro-->
-	</section><!--/.history-->
+	<div class="grid">
 	
-	<section class="about">
-		<div class="intro row" id="contain">
-			<div class="col-sm-8">
-					<div class="entry-content">
-					  <p><a href="<?php bloginfo('url'); ?>">Click here to return to the homepage</a>.</p>
-					</div>
-			</div><!--/.col-->
-			
-			<div class="col-sm-4 latest-news">
-				<?php get_template_part( 'part', 'latest_news' ); ?>
-			</div><!--/.col-->
-		</div><!--/.intro-->
-	</section><!--/.about-->
-	</div><!--/.page-about-->
+	<div id="secondary" class="classes-sidebar widget-area col-3-12" role="complementary">
+		<h3 class="page-title animate fadeIn">Search the Site</h3>
+		<p class="info">Select an option below to filter content within the website. Content searched includes news, events &amp; classes.</p>
+		  <?php echo do_shortcode('[ULWPQSF id=66 formtitle="0" button=0]'); ?>
+	</div><!-- .sidebar --> 
+	
+		<div id="primary" class="col-9-12">
+			<main id="main" class="site-main entry-content" role="main">
 
+			<div class="class-list">
+			  <div class="results-total">
+				<h3>Page Not Found - 404. </h3>
+				<p>The page you are looking for can not be found. Perhaps try searching, or use the filter in the sidebar to help you search the site.</p>
+				<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+			    <div><label class="screen-reader-text" for="s">Search:</label>
+			        <input type="text" value="" name="s" id="s" />
+			        <input type="submit" id="searchsubmit" value="Search" />
+			    </div>
+			</form>
+			  </div><!--/.results-total-->
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'search' ); ?>
+
+			<?php endwhile; // end of the loop. ?>
+			</div><!--/.class-list-->
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+
+	</div><!-- grid -->
 	<?php get_footer(); ?>
