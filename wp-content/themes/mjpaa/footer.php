d6bdcdb1da99ffcc4e95f66e69fad0b81cd5ea2b
brianotoole <botoole518@gmail.com>
2015-08-18 21:49:24 -0400

    added footer-top & footer-bottom

--- wp-content/themes/mjpaa/footer.php
+++ wp-content/themes/mjpaa/footer.php
@@ -16,9 +16,57 @@
 	</section><!--/#content --> 
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-    	<div class="grid grid-pad">
-			<div class="site-info col-1-1">
-				&copy; Copyright <?php echo date('Y'); ?><em> <br class="visible-xs" /><?php bloginfo('blog_name'); ?></em> &nbsp; <br class="visible-sm visible-xs" /> <a href="<?php bloginfo('url'); ?>/about"> About</a> <a href="<?php bloginfo('url'); ?>/contact"> Contact</a> <a id="top" href="#"> Back To Top</a>
+	
+		<div class="footer-top grid grid-pad">
+			<div class="award col-3-12">
+				award
+			</div><!-- .site-info -->
+			<div class="links col-3-12">
+				<h6>Links</h6>
+				<ul>
+					<li><i class="fa fa-chevron-right"></i><a href="">Press</a></li>
+					<li><i class="fa fa-chevron-right"></i><a href="">Careers</a></li>
+					<li><i class="fa fa-chevron-right"></i><a href="">Donation Requests</a></li>
+					<li><i class="fa fa-chevron-right"></i><a href="">FAQ</a></li>
+					<li><i class="fa fa-chevron-right"></i><a href="">Gift Cards</a></li>
+				</ul>
+			</div><!-- .site-info -->
+			<div class="family col-3-12">
+				<h6>MJPAA Family</h6>
+				<ul>
+					<li><i class="fa fa-chevron-right"></i><a href="http://tbttt.org" target="_blank">Tampa Bay Triple Threat Theatre</a></li>
+					<li><i class="fa fa-chevron-right"></i></i><a href="#">Classical Ballet of Tampa</a></li>
+			</div><!-- .site-info -->
+			<div class="ph col-3-12">
+				<h6>Contact</h6>
+					<p>
+						<a href="tel:#">813-555-5555</a>
+					</p>
+					<p>
+						1234 Elm St. Tampa, FL
+					</p>
+			</div><!-- .site-info -->
+        </div><!-- grid --> 
+        
+    	<div class="footer-bottom grid grid-pad">
+			<div class="site-info col-12-6">
+				&copy; <?php echo date('Y'); ?> MJPAA.com<em> | <a id="top" href="#"> Back To Top</a>
+			</div><!-- .site-info -->
+			<div class="social-media col-6-12">
+				<ul>
+					<a href="" target="_blank">
+						<li><i class="fa fa-facebook"></i></li>
+					</a>
+					<a href="" target="_blank">
+						<li><i class="fa fa-twitter"></i></li>
+					</a>
+					<a href="" target="_blank">
+						<li><i class="fa fa-instagram"></i></li>
+					</a>
+					<a href="" target="_blank">
+						<li><i class="fa fa-youtube"></i></li>
+					</a>
+				</ul>
 			</div><!-- .site-info -->
         </div><!-- grid --> 
 	</footer><!-- #colophon -->
