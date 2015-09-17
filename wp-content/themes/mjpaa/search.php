eabd1e703e87939e65aae2b34905242ded853d04
brianotoole <botoole518@gmail.com>
2015-09-17 15:46:41 -0400

    removed filter from search results

--- wp-content/themes/mjpaa/search.php
+++ wp-content/themes/mjpaa/search.php
@@ -5,7 +5,7 @@ Template for displaying search results.
 get_header(); ?> 
 
 	<div class="grid">
-		<div id="primary" class="col-9-12 pull-right">
+		<div id="primary" class="col-1-1 pull-right">
 			<main id="main" class="site-main entry-content" role="main">
 
 			  <div class="results-total">
@@ -29,11 +29,5 @@ get_header(); ?>
 			</main><!-- #main -->
 		</div><!-- #primary -->
 		
-	<div id="secondary" class="classes-sidebar widget-area col-3-12 pull-left" role="complementary">
-		<h3 class="page-title animate fadeIn">Search the Site</h3>
-		<p class="info">Select an option below to filter content within the website.</p>
-		  <?php echo do_shortcode('[ULWPQSF id=66 formtitle="0" button=0]'); ?>
-	</div><!-- .sidebar --> 
-
 	</div><!-- grid -->
 	<?php get_footer(); ?>
