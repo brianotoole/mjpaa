a2e795294fc2868d35d6408ed135fb03d9639033
brianotoole <botoole518@gmail.com>
2016-02-09 19:48:41 -0500

    summer landing setup & inital layout

---
+++ wp-content/themes/mjpaa/page-landing-summer.php
@@ -0,0 +1,159 @@
+<?php
+/**
+Template Name: Landing, Summer
+ *
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Summer</title>
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="icon" href="img/favicon.png" type="image/x-icon">
+    <link rel="apple-touch-icon" href="img/favicon.png">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lustria:100,400|Lato,300,400" type="text/css" media="all" />
+</head>
+<body>
+
+<header>
+  <nav class="landing-nav">
+    <div class="contain">
+      <a href="/" class="logo" rel="logo">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/img/logo.png">
+      </a>
+      <ul class="menu">
+        <a class="" href="#">Visit MJPAA.com</a>
+      </ul>
+    </div>
+  </nav>
+</header>
+
+<section class="landing-hero clipped">
+  <div class="contain center">
+    <h1 class="title">Sweet Summer Time.</h1>
+    <h4><span>It's that time of the year again at MJPAA.<br />Register for classes today!</span></h4>
+    <a href="" class="button border new">I am a New Student</a>
+    <a href="" class="button border current">I am a Current Student</a>
+  </div>
+  <!--<div class="page-scroll js-page-scroll">
+    <i class="fa fa-arrow-down"></i>
+  </div>-->
+</section>
+
+<section class="landing-intro">
+  <div class="contain center padded">
+    <h2 class="title has-emblem">Our Summer Program offers</h2>
+      <div class="emblem">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/img/icon-courses.png">
+      </div>
+      <div class="row">
+      
+      
+        <div class="col-sm-4">
+          <ul class="course-list">
+			<li class="clearfix">
+			  <div class="course pull-left">
+			    <img src="https://cdn.messagebird.com/frontend-assets/images/illustrations/icons/sms-gateway.svg">
+			  </div>
+			Dance &amp; Classical Ballet
+			</li>
+			<li class="clearfix">
+			  <div class="course pull-left">
+			    <img src="https://cdn.messagebird.com/frontend-assets/images/illustrations/icons/sms-gateway.svg">
+			  </div>
+			Acting &amp; Theater
+			</li>
+		  </ul><!--/.course-list-->
+        </div><!--/.col-->
+        
+        <div class="col-sm-4">
+          <ul class="course-list">
+			<li class="clearfix">
+			  <div class="course pull-left">
+			    <img src="https://cdn.messagebird.com/frontend-assets/images/illustrations/icons/sms-gateway.svg">
+			  </div>
+			Music, Instruments &amp; Voice
+			</li>
+			<li class="clearfix">
+			  <div class="course pull-left">
+			    <img src="https://cdn.messagebird.com/frontend-assets/images/illustrations/icons/sms-gateway.svg">
+			  </div>
+			Acrobatics
+			</li>
+		  </ul><!--/.course-list-->
+        </div><!--/.col-->
+        
+        <div class="col-sm-4">
+          <ul class="course-list">
+			<li class="clearfix">
+			  <div class="course pull-left">
+			    <img src="https://cdn.messagebird.com/frontend-assets/images/illustrations/icons/sms-gateway.svg">
+			  </div>
+			Classical Ballet
+			</li>
+			<li class="clearfix">
+			  <div class="course pull-left">
+			    <img src="https://cdn.messagebird.com/frontend-assets/images/illustrations/icons/sms-gateway.svg">
+			  </div>
+			And many others!
+			</li>
+		  </ul><!--/.course-list-->
+        </div><!--/.col-->
+      </div><!--/.row-->
+  </div><!--/.contain-->
+</section>
+
+<secion class="landing-memories">
+  <div class="contain center padded">
+    <!--<h2 class="title has-emblem">Summer Memories</h2>
+      <div class="emblem">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/img/icon-news.png">
+      </div>-->
+	<div class="row">
+		<div class="col-sm-12 images">
+		  <div class="col-sm-2"></div>
+		  <div class="col-sm-2 stack rotated-left">
+	        	<img src="http://mjpaa.com/wp-content/uploads/2015/08/memories1.jpg">
+	      </div>
+	      <div class="col-sm-2 stack rotated-middle">
+	        	<img src="http://mjpaa.com/wp-content/uploads/2015/08/memories2.jpg">
+	      </div>
+	      <div class="col-sm-2 stack rotated-right">
+	        	<img src="http://mjpaa.com/wp-content/uploads/2015/08/memories3.jpg">
+	      </div>
+	      <div class="col-sm-2"></div>
+		</div>
+	</div>
+  </div>
+</secion>
+
+<section class="landing-quote">
+  <div class="contain center not-padded">
+    <i></i> "When the arts enter a child's world, magic happens." <em>- Mary Jo Scanio, Founder</em>
+  </div>
+</section>
+
+<section class="landing-callout">
+  <div class="contain center padded">
+    <h2 class="title has-emblem">Ready to Get Started</h2>
+      <div class="emblem">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/img/icon-courses.png">
+      </div>
+    
+    <div class="buttons">
+      <a href="" class="button border new">Find Summer Classes</a>
+      <a href="" class="button border current">View Summer Brochure</a>
+    </div>
+    
+  </div>
+</section>
+
+
+
+</body>
+</html>
