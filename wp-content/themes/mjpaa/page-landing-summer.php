78e64510af461e6f7f82f355eae107d85df4988b
brianotoole <botoole518@gmail.com>
2016-02-11 02:46:48 -0500

    grouped classes by session

--- wp-content/themes/mjpaa/page-landing-summer.php
+++ wp-content/themes/mjpaa/page-landing-summer.php
@@ -26,11 +26,15 @@ Template Name: Landing, Summer
       <a href="<?php bloginfo('siteurl'); ?>" class="logo" rel="logo" title="MJPAA">
         <img src="<?php bloginfo('stylesheet_directory'); ?>/img/logo.png">
       </a>
-      <!--<ul class="menu">
-        <a class="visit" href="<?php bloginfo('siteurl'); ?>">Visit MJPAA.com</a>
-      </ul>-->
+      <ul class="menu">
+        <a href="https://app.jackrabbitclass.com/reg.asp?id=505581" target="_blank">New Student Registration</a>
+        <a href="https://app.jackrabbitclass.com/portal/ppLogin.asp?id=505581" target="_blank">Customer Login</a>
+      </ul>
     </div>
   </nav>
+</header>
+
+<section class="landing-hero clipped">
 <div class="sun">
             <div class="sun-face">
                 <div class="sun-hlight"></div>
@@ -87,96 +91,17 @@ Template Name: Landing, Summer
                 <s></s>
             </div>
         </div>
-    </div>
-</header>
-
-<section class="landing-hero clipped">
-
-<div class="triangle one"></div>
-<div class="triangle two"></div>
-<div class="triangle three"></div>
-<div class="triangle four"></div>
-<div class="triangle five"></div>
+    </div><!--/.sun-->
   <div class="contain center">
-    <h1 class="title">Summer is Coming</h1>
-    <h4>Don't miss out on our early bird Summer Registration Discounts.<br />Register for classes today.</span></h4>
-    <a href="" class="button border l">Register Now</a>
+    <h2 class="title">Discover the Difference</h2>
+    <h6>Don't miss out on our early bird Summer Registration Discounts.<br />Register for classes today.</span></h6>
     <a href="" class="button border r">Find Summer Classes</a>
+    <a href="" class="button border r">View Summer Brochure</a>
   </div>
-  <!--<div class="page-scroll js-page-scroll">
-    <i class="fa fa-arrow-down"></i>
-  </div>-->
 </section>
 
-<section class="landing-intro">
+<section class="landing-memories">
   <div class="contain center padded">
-    <h2 class="title has-emblem">Our Summer curriculum</h2>
-      <div class="emblem">
-        <img src="<?php bloginfo('stylesheet_directory'); ?>/img/icon-courses.png">
-      </div>
-      <div class="row">
-      
-      
-        <div class="col-sm-4">
-          <ul class="course-list">
-			<li class="clearfix">
-			  <div class="course pull-left">
-			    <i class="fa fa-check"></i>
-			  </div>
-			Dance
-			</li>
-			<li class="clearfix">
-			  <div class="course pull-left">
-			    <i class="fa fa-check"></i>
-			  </div>
-			Classical Ballet
-			</li>
-		  </ul><!--/.course-list-->
-        </div><!--/.col-->
-        
-        <div class="col-sm-4">
-          <ul class="course-list">
-			<li class="clearfix">
-			  <div class="course pull-left">
-			    <i class="fa fa-check"></i>
-			  </div>
-			Music, Instruments &amp; Voice
-			</li>
-			<li class="clearfix">
-			  <div class="course pull-left">
-			    <i class="fa fa-check"></i>
-			  </div>
-			Acting &amp; Theater
-			</li>
-		  </ul><!--/.course-list-->
-        </div><!--/.col-->
-        
-        <div class="col-sm-4">
-          <ul class="course-list">
-			<li class="clearfix">
-			  <div class="course pull-left">
-			    <i class="fa fa-check"></i>
-			  </div>
-			Acrobatics
-			</li>
-			<li class="clearfix">
-			  <div class="course pull-left">
-			    <i class="fa fa-check"></i>
-			  </div>
-			... and more!
-			</li>
-		  </ul><!--/.course-list-->
-        </div><!--/.col-->
-      </div><!--/.row-->
-  </div><!--/.contain-->
-</section>
-
-<secion class="landing-memories">
-  <div class="contain center padded">
-    <!--<h2 class="title has-emblem">Summer Memories</h2>
-      <div class="emblem">
-        <img src="<?php bloginfo('stylesheet_directory'); ?>/img/icon-news.png">
-      </div>-->
 	<div class="row">
 		<div class="col-sm-12 images">
 		  <div class="col-sm-2"></div>
@@ -193,7 +118,103 @@ Template Name: Landing, Summer
 		</div>
 	</div>
   </div>
-</secion>
+</section>
+
+<section class="landing-intro camps">
+  <div class="contain center padded">
+    <h2 class="title has-emblem">Summer Camps &amp; Workshops</h2>
+      <div class="emblem">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/img/icon-courses.png">
+      </div>
+      
+<?php
+// get all summer classes
+$args = array (
+  'post_type' => 'class',
+    'tax_query' => array(
+        'relation' => 'AND',
+        array(
+            'taxonomy' => 'session',
+            'field' => 'slug',
+            'terms' => array( 'summer-camps'),
+            'operator' => 'IN'
+        ),
+  'order'		   => 'DESC',
+  'posts_per_page' => -1, 
+  'post_status' => 'publish'
+  )
+);
+// the loop
+$query = new WP_Query($args); 
+  if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); 
+?>
+<div class="classes-wrapper">
+  <div class="class">
+    <a href="<?php the_permalink() ?>">
+      <h5><?php the_title(); ?></h5>
+	</a>
+	<a href="<?php the_permalink() ?>">
+	  <p>View Class <i class="fa fa-angle-double-right"></i></p>
+	</a>
+  </div><!--/.class-->
+</div><!--/.class-wrapper-->
+
+<?php endwhile; 
+ wp_reset_postdata();
+ else : ?>
+<p><?php _e( 'There are no classes posted at this time. Please check back later.' ); ?></p>
+<?php endif; ?>      
+
+  </div><!--/.contain-->
+</section>
+
+<section class="landing-intro classes">
+  <div class="contain center padded">
+    <h2 class="title has-emblem">Summer Classes</h2>
+      <div class="emblem">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/img/icon-courses.png">
+      </div>
+      
+<?php
+// get all summer classes
+$args = array (
+  'post_type' => 'class',
+    'tax_query' => array(
+        'relation' => 'AND',
+        array(
+            'taxonomy' => 'session',
+            'field' => 'slug',
+            'terms' => array( 'summer-classes'),
+            'operator' => 'IN'
+        ),
+  'order'		   => 'DESC',
+  'posts_per_page' => -1, 
+  'post_status' => 'publish'
+  )
+);
+// the loop
+$query = new WP_Query($args); 
+  if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); 
+?>
+<div class="classes-wrapper">
+  <div class="class">
+    <a href="<?php the_permalink() ?>">
+      <h5><?php the_title(); ?></h5>
+	</a>
+	<a href="<?php the_permalink() ?>">
+	  <p>View Class <i class="fa fa-angle-double-right"></i></p>
+	</a>
+  </div><!--/.class-->
+</div><!--/.class-wrapper-->
+
+<?php endwhile; 
+ wp_reset_postdata();
+ else : ?>
+<p><?php _e( 'There are no classes posted at this time. Please check back later.' ); ?></p>
+<?php endif; ?>      
+
+  </div><!--/.contain-->
+</section>
 
 <section class="landing-quote">
   <div class="contain center not-padded">
@@ -209,15 +230,28 @@ Template Name: Landing, Summer
       </div>
     
     <div class="buttons">
-      <a href="" class="button border new">Find Summer Classes</a>
-      <a href="" class="button border current">View Summer Brochure</a>
+      <a href="" class="button border l">Find Summer Classes</a>
+      <a href="" class="button border r">Register Now</a>
     </div>
     
   </div>
 </section>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script>
+/**
+ * header scroll
+ */
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if ( scroll >= 40 && $(window).width() >= 899) { //not on mobile
+    	$('.landing-nav').slideDown(300);
+        $('.landing-nav').addClass("fixed");
+   } else {
+	    $('.landing-nav').removeClass("fixed");
+   }
 
-
-
+});
+</script>
 
 </body>
 </html>
