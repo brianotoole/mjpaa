7b6a8ee6c2e3957b73da124bd4e7171e7da8ef83
brianotoole <botoole518@gmail.com>
2016-03-04 16:23:23 -0500

    undo

--- wp-content/themes/mjpaa/functions.php
+++ wp-content/themes/mjpaa/functions.php
@@ -117,7 +117,6 @@ function mjpaa_scripts() {
    
 	
 	//wp_enqueue_script('jquery');
-	wp_deregister_script( 'jquery' );
 	wp_enqueue_script( 'site-scripts', get_template_directory_uri() . '/js/site.min.js', array(), '1.0.0', true );
 	
 	//wp_enqueue_script( 'skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
